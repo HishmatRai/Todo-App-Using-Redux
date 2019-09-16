10fa01220366a800743db6a574e7abac9448281c
HishmatRai <hishmatrai.2018@gmail.com>
2019-09-17 02:20:00 +0500

    Todo-App-Using-Redux

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {add,delTodo,editTodo,cancel,update} from './../../Store/action'
+import {add,delTodo,editTodo,cancel,update} from './../../Store/action';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { async } from 'q';
 class Home extends React.Component{
     constructor(){
         super()
@@ -16,33 +19,57 @@ this.setState ({
     name:name
 })
     }
+ addValue=async()=>{
+    await this.props.add(this.state);
+    this.setState({
+        name:""
+    })
+ }
+
     render(){
         console.log(this.props.Todo.name[0])
         console.log(this.state);
         return(
             <div>
-                <h1>Home</h1>
-                {/* <h1>{this.props.Todo.name}</h1> */}
-                <input type = "text" onChange = {(e) => this.change(e)}/>
-                <button onClick = { ()=> this.props.add(this.state)}> add</button>
+               <Paper>
+                   <h1 style={{textAlign:"center",color:"#1976d2"}}>WelCome To Todo</h1>
+               </Paper>
+               
+<Paper>
+                <input value={this.state.name} style={{width:"80%",margin:"2% 1% 2% 5%",padding:"8px",borderRadius:"5px",outline:"none",border:"2px solid #1976d2"}} type = "text" placeholder="Type here ..." onChange = {(e) => this.change(e)}/>
+                <Button style={{backgroundColor:"#1976d2",color:"white",margin:"10px"}} onClick = { ()=> this.addValue()}>add </Button>
 
+                {/* <button onClick = { ()=> this.props.add(this.state)}> add</button> */}
 
-                {this.props.Todo.name && this.props.Todo.name.map ((val,ind) => <li>
+                </Paper>
+            
+                    <Paper style={{width:"80%",margin :'0 auto',marginTop:"20px"}}>
+                {this.props.Todo.name && this.props.Todo.name.map ((val,ind) => <li style={{listStyle:"none"}}>
                 {
                     val.value?
-                    <div>
-                        <span>{val.name}</span>
-                <button onClick={()=>this.props.editTodo(ind)}>Edit</button>
-                <button onClick={()=>this.props.delTodo(ind)}>Delete</button>
+                    <div style={{borderBottom:"1px solid #1976d2",padding:"16px"}}>
+                   
+
+                        <span style={{fontSize:"20px",marginLeft:"20px" }}>{val.name}</span>
+                   
+                        <Button style={{backgroundColor:"#1976d2",color:"white",margin:"10px",padding:"0px",float:"right"}} onClick={()=>this.props.editTodo(ind)}>edit</Button>
+                        <Button style={{backgroundColor:"#1976d2",color:"white",margin:"10px",padding:"0px",float:'right'}} onClick={()=>this.props.delTodo(ind)}>delete</Button>
+                {/* <button onClick={()=>this.props.editTodo(ind)}>Edit</button> */}
+                {/* <button onClick={()=>this.props.delTodo(ind)}>Delete</button> */}
                 </div>
                 :
-                <div>
-                    <input type="text" onChange={(e)=>this.setState({updValue:e.target.value})}/>
-                <button onClick={()=>this.props.update(ind,this.state.updValue)}>update</button>
-                <button onClick={()=>this.props.cancel(ind)}>cancel</button>
+                <div  style={{borderBottom:"1px solid #1976d2",padding:"10px"}}>
+                    <input style={{width:"82%",padding:"3px",borderRadius:"5px",outline:"none",border:"2px solid #1976d2"}} type="text" placeholder="Update todo ...." onChange={(e)=>this.setState({updValue:e.target.value})}/>
+                {/* <button onClick={()=>this.props.update(ind,this.state.updValue)}>update</button> */}
+                <Button style={{backgroundColor:"#1976d2",color:"white",margin:"10px",padding:"0px"}}  onClick={()=>this.props.update(ind,this.state.updValue)}>update</Button>
+
+                {/* <button onClick={()=>this.props.cancel(ind)}>cancel</button> */}
+                <Button style={{backgroundColor:"#1976d2",color:"white",margin:"10px",padding:"0px"}} onClick={()=>this.props.cancel(ind)}>cancel</Button>
+                
                 </div>
             }
                 </li>)}
+                </Paper>
             </div>
         )
     }
